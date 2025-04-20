92547327f535aadd3d03f8348b70aee34484b817
ZiyadZK <kakangtea74@gmail.com>
2025-04-20 18:48:30 +0700

    recalibrate QR Code

--- resources/js/Pages/absenqr.jsx
+++ resources/js/Pages/absenqr.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react"
 import { useSidebar } from "../context/SidebarContext"
-import { createTheme, IconButton, useMediaQuery } from "@mui/material"
+import { CircularProgress, createTheme, Fade, Grow, IconButton, useMediaQuery } from "@mui/material"
 import MainLayout from "../layouts/MainLayout"
-import { MenuOutlined } from "@mui/icons-material"
+import { Check, Close, MenuOutlined } from "@mui/icons-material"
 import QRScanner from "../components/CustomQRCode"
 import { Scanner } from "@yudiel/react-qr-scanner"
+import api_handler from "../libs/api_handler"
 
 export default function AbsenQR({ token, base_url, role}) {
 
     const { setShowSidebar } = useSidebar()
-    const [data, setData] = useState(null)
     const theme = createTheme({
         components: {
             MuiUseMediaQuery: {
@@ -20,13 +20,71 @@ export default function AbsenQR({ token, base_url, role}) {
         }
     })
 
+    const [listData, setListData] = useState({
+        absen: {
+            loading: false,
+            error: null,
+            success: false
+        }
+    })
+
+    const aksi = {
+        absen: {
+            submit: async (data) => {
+                try {
+
+                    if(!data) return
+
+                    const [kelas, pin, unique] = data.split('-')
+
+                    if(!kelas || !pin || !unique) return
+
+                    aksi.absen.set('error', null)
+                    aksi.absen.set('loading', true)
+
+                    const response = await api_handler.get({
+                        base_url,
+                        token,
+                        url: `kelas-kuliah/mahasiswa/presensi/qrcode?kelas=${kelas}&pin=${pin}${unique === '1' ? '&unique_pin=true' : ''}`
+                    })
+
+                    aksi.absen.set('loading', false)
+
+                    if(response?.success) {
+                        aksi.absen.set('success', true)
+                        setTimeout(() => {
+                            aksi.absen.set('success', false)
+                        }, 5000)
+                    }else{
+                        aksi.absen.set('error', response?.message)
+                        setTimeout(() => {
+                            aksi.absen.set('error', null)
+                        }, 5000)
+                    }
+                } catch (error) {
+                    aksi.absen.set('loading', false)
+                    aksi.absen.set('success', false)
+                    aksi.absen.set('error', error?.message || "Terjadi kesalahan saat memproses.")
+                    setTimeout(() => {
+                        aksi.absen.set('error', null)
+                    }, 5000)
+                }
+            },
+            set: (column, value) => {
+                setListData(state => ({
+                    ...state,
+                    absen: {
+                        ...state.absen,
+                        [column]: value
+                    }
+                }))
+            }
+        }
+    }
+
     const matches = {
         lg: useMediaQuery(theme.breakpoints.up('lg'))
     }
-
-    useEffect(() => {
-        console.log(data)
-    }, [data])
 
     return (
         <MainLayout token={token} base_url={base_url} role={role}>
@@ -77,16 +135,65 @@ export default function AbsenQR({ token, base_url, role}) {
                                         </p>
                                     </div>
                                 </div>
-                                <QRScanner
-                                    onScanned={(data) => console.log({ data })}
-                                    facingMode={'environment'}
-                                    onError={(error) => console.log({ error })}
-                                />
-                                {/* <Scanner onScan={(data) => console.log({ data })} onError={(error) => console.log({ error })} /> */}
-
-                                <div className="p-4">
-                                    {data}
-                                </div>
+                                {listData.absen.loading
+                                    ? (
+                                        <div className="p-4">
+                                            <div className="w-full aspect-square flex items-center justify-center">
+                                                <Grow in timeout={1000} style={{ transformOrigin: '0 12 0' }}>
+                                                    <CircularProgress size={50} />
+                                                </Grow>
+                                            </div>
+                                        </div>
+                                    ): listData.absen.success || listData.absen.error ? (
+                                        <div className="p-4">
+                                            <div className="w-full aspect-square flex items-center justify-center">
+                                                {listData.absen.success
+                                                    ? (
+                                                        <div className="flex flex-col items-center gap-4">
+                                                            <Grow in timeout={1000} style={{ transformOrigin: '0 12 0' }}>
+                                                                <Check sx={{ fontSize: 120 }} color="success" />
+                                                            </Grow>
+                                                            <Grow in timeout={1000}  style={{ transformOrigin: '12 0 0', transitionDelay: '500ms' }}>
+                                                                <h1 className="text-4xl font-extrabold tracking-tighter text-green-700">
+                                                                    Yay!
+                                                                </h1>
+                                                            </Grow>
+                                                            <Grow in timeout={1000}  style={{ transformOrigin: '12 0 0', transitionDelay: '500ms' }}>
+                                                                <p className="text-center text-lg tracking-tighter opacity-70">
+                                                                    Anda telah berhasil melakukan Presensi
+                                                                </p>
+                                                            </Grow>
+                                                        </div>
+                                                    ):(
+                                                        <div className="flex flex-col items-center gap-4">
+                                                            <Grow in timeout={1000} style={{ transformOrigin: '0 12 0' }}>
+                                                                <Close sx={{ fontSize: 120 }} color="error" />
+                                                            </Grow>
+                                                            <Grow in timeout={1000}  style={{ transformOrigin: '12 0 0', transitionDelay: '500ms' }}>
+                                                                <h1 className="text-4xl font-extrabold tracking-tighter text-red-700">
+                                                                    Oops!
+                                                                </h1>
+                                                            </Grow>
+                                                            <Grow in timeout={1000}  style={{ transformOrigin: '12 0 0', transitionDelay: '500ms' }}>
+                                                                <p className="text-center text-lg tracking-tighter opacity-70">
+                                                                    {listData.absen.error}
+                                                                </p>
+                                                            </Grow>
+                                                        </div>
+    
+                                                    )
+                                                }
+                                            </div>
+                                        </div>
+                                    ):(
+                                        <QRScanner
+                                            onScanned={(data) => {
+                                                if (!listData.absen.loading) aksi.absen.submit(data)
+                                            }}
+                                            facingMode={'environment'}
+                                            onError={(error) => console.log({ error })}
+                                        />
+                                    )}
                             </>
                         )
                     }
